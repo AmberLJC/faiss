52aa8d6072b2e66626a9521dcac5c191fbc78626
AmberLJC <amberljc@umich.edu>
2019-07-29 21:13:22 -0400

    fix order

--- compile.sh
+++ compile.sh
@@ -1,4 +1,10 @@
- ./configure --prefix=/home/amberljc/faiss --without-cuda 
+
+echo "export CPLUS_INCLUDE_PATH=$CPLUS_INLCUDE_PATH:/home/amberljc/faiss
+export LIBRARY_PATH=$LIBRARY_PATH:/home/amberljc/faiss
+export LD_LIBRARY_PATH=/usr/local/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/amberljc/faiss">>~/.bash_profile
+
+./configure --prefix=/home/amberljc/faiss --without-cuda 
 
 make demos -j 30
 make install
@@ -13,9 +19,5 @@ mkdir pipeline/
 mkdir res/
 
 
-echo "export CPLUS_INCLUDE_PATH=$CPLUS_INLCUDE_PATH:/home/amberljc/faiss
-export LIBRARY_PATH=$LIBRARY_PATH:/home/amberljc/faiss
-export LD_LIBRARY_PATH=/usr/local/lib64/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/amberljc/faiss">>~/.bash_profile
 
 
